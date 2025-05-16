9356b60787067e9e29f769d352c003555d50ef9a
MegatHub <yanimran152@gmail.com>
2025-05-16 17:20:31 +0800

    Add files via upload

---
+++ elevate2025/main/js/elevate.js
@@ -0,0 +1,31 @@
+function toggleMode() {
+      document.body.classList.toggle('dark-mode');
+    }
+// Dark Mode Toggle
+
+    window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
